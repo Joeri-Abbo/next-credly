9a4caa09d1e0eb0ba8a70d7fa69a947d9ea02049
Joeri Abbo <joeriabbo@hotmail.com>
2023-04-01 23:09:20 +0200

    Refactor

--- components/BadgeList.tsx
+++ components/BadgeList.tsx
@@ -1,59 +1,5 @@
 import {useState, useEffect} from "react";
-
-interface Badge {
-    id: string;
-    cost: number | null;
-    description: string;
-    global_activity_url: string;
-    earn_this_badge_url: string | null;
-    enable_earn_this_badge: boolean;
-    enable_detail_attribute_visibility: boolean;
-    level: string;
-    name: string;
-    vanity_slug: string;
-    time_to_earn: string | null;
-    type_category: string;
-    show_badge_lmi: boolean;
-    show_skill_tag_links: boolean;
-    translatable: boolean;
-    image: {
-        id: string;
-        url: string;
-    };
-    image_url: string;
-    url: string;
-    issuer: {
-        summary: string;
-        entities: {
-            label: string;
-            primary: boolean;
-            entity: {
-                type: string;
-                id: string;
-                name: string;
-                url: string;
-                vanity_url: string;
-                internationalize_badge_templates: boolean;
-                share_to_ziprecruiter: boolean;
-                verified: boolean;
-            };
-        }[];
-    };
-    alignments: unknown[];
-    badge_template_activities: {
-        id: string;
-        activity_type: string;
-        required_badge_template_id: string | null;
-        title: string;
-        url: string | null;
-    }[];
-    endorsements: unknown[];
-    skills: {
-        id: string;
-        name: string;
-        vanity_slug: string;
-    }[];
-}
+import Badge from "./BadgeInterface";
 
 export default function BadgeList() {
     const [badges, setBadges] = useState<Badge[]>([]);
@@ -71,7 +17,7 @@ export default function BadgeList() {
     useEffect(() => {
         async function fetchBadges() {
             const response = await fetch(
-                "https://raw.githubusercontent.com/Joeri-Abbo/python-credly-scraper/master/data/badges.json"
+                "https://raw.githubusercontent.com/Joeri-Abbo/python-credly-scraper/2965afc5ff6a0575821a90d72e4e82288fb8ce24/data/badges.json"
             );
             const data = await response.json();
             console.log(data); // check the data
